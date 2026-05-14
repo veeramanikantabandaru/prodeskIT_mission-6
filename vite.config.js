ee61158948261845a4705a5eeeca5bfcf883331a
veeramanikantabandaru <veeramanikantabandaru22@gmail.com>
2026-05-15 00:20:08 +0530

    Fix: Support Vercel deployment with conditional base path and routing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/prodeskIT_mission-6/",
+  base: process.env.GITHUB_ACTIONS ? "/prodeskIT_mission-6/" : "/",
 })
